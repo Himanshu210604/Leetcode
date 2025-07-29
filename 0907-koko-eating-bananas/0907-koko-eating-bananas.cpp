9590a9c44abf8ca3aa4d863dbf1f9c69fa390bfb
Himanshu210604 <himanshurathoremorar@gmail.com>
2025-07-29 19:13:24 +0530

    Time: 9 ms (50.97%), Space: 22.8 MB (73.79%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -4,10 +4,11 @@ public:
      int low = 1 ; 
  int high = *max_element(piles.begin(), piles.end()); 
      while(low<=high) {
-        int mid= (low+high)/2 ; 
+        int mid= (low+high)/2 ;  
+        int n = piles.size() ; 
         long long time = 0 ; 
-        for(int i = 0 ; i< piles.size() ; i++) {
-            time+=(long long)(piles [i] + mid -1)/mid ; 
+        for(int i = 0 ; i< n ; i++) {
+            time+=(long long)(piles[i] + mid -1)/mid ; 
         } 
         if(time<=h) {
             high=mid-1 ; 
