38fcadf571d9157dd0f77b597ec72d17bece992e
Himanshu210604 <himanshurathoremorar@gmail.com>
2025-08-13 00:00:27 +0530

    Time: 0 ms (100%), Space: 17.1 MB (22.94%) - LeetHub

--- 2508-maximum-sum-of-an-hourglass/2508-maximum-sum-of-an-hourglass.cpp
+++ 2508-maximum-sum-of-an-hourglass/2508-maximum-sum-of-an-hourglass.cpp
@@ -5,8 +5,8 @@ public:
         int c = grid[0].size(); 
         int maxSum = 0;
 
-        for (int i = 0; i < r - 2; i++) {
-            for (int j = 0; j < c - 2; j++) {
+        for (int i = 0; i <= r - 3; i++) {
+            for (int j = 0; j <= c - 3; j++) {
                 int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2] 
                         + grid[i+1][j+1] 
                         + grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2];   
