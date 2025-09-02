8c54c7c5ae0c6aa297b996dd4b03be2b41a699d0
Himanshu210604 <himanshurathoremorar@gmail.com>
2025-09-02 22:52:31 +0530

    Time: 11 ms (43.2%), Space: 71.3 MB (92.63%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -4,7 +4,7 @@ public:
         int l = 0 , r = 0 , sum = 0 , cnt = 0;  
         if (k < 0) return 0; 
         while (r < nums.size()) {
-            sum += (nums[r] % 2);   // count odd numbers
+            sum += (nums[r] % 2);   
             while (sum > k) { 
                 sum -= (nums[l] % 2); 
                 l++; 
