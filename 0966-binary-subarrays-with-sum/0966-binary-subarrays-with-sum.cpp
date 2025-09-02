b73fd0113c3b92137ced2acb29e0db3493abf00e
Himanshu210604 <himanshurathoremorar@gmail.com>
2025-09-02 22:39:31 +0530

    Time: 0 ms (100%), Space: 32.6 MB (59.35%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int solver(vector<int>& nums, int goal) {   
         int l = 0 , r = 0 , sum = 0 , cnt = 0 ;  
-        if (goal < 0) return 0;  // also handle negative case
+        if (goal < 0) return 0;  
         while (r < nums.size()) {
             sum += nums[r]; 
             while (sum > goal) { 
